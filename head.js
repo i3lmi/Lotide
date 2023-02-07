e40db8cdf10817689bba52ee9d6c48a823c73a70
i3lmi <idriselmi99@gmail.com>
2023-02-07 06:47:47 +0000

    implented logic to get first element of an array , also fixed assert equal function

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected} `);
+  }
+};
+
+let array = ["first", "second", "third", "fourth", "fifth"];
+const head = function (array) {
+  if (array === []) {
+    return undefined;
+  }
+  let firstIndex = array[0];
+  return firstIndex;
+};
+
+assertEqual(head([5, 6, 7]), 6);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 6);
