73ae43b4e4f1ddb75d9254f83740d7dde0679ae6
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-09 19:01:08 -0500

    without.js

---
+++ without.js
@@ -0,0 +1,27 @@
+function assertArraysEqual(array1, array2) {
+    if (eqArrays(array1, array2)) {
+      console.log("The arrays are equal.");
+    } else {
+      console.log("The arrays are not equal.");
+    }
+  }
+
+  assertArraysEqual([1, 2, 3], [1, 2, 3]); // logs "The arrays are equal."
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // logs "The arrays are not equal."
+
+
+  function eqArrays(array1, array2) {
+    if (array1.length !== array2.length) {
+      return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let originalArray = [1, 2, 3];
+without(originalArray, 1);
+assertArraysEqual(originalArray, [1, 2, 3]);
