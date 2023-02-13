8b90a19bad1e534377dadb42c2c5dc6c22d41255
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-13 02:24:37 -0500

    takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,34 @@
+const takeUntil = function(array, callback) {
+
+  }
+
+  const takeUntil = function(array, callback) {
+    const result = [];
+    for (const item of array) {
+      if (callback(item)) {
+        return result;
+      }
+      result.push(item);
+    }
+    return result;
+  };
+  
+  const takeUntil = function(array, callback) {
+    const result = [];
+    for (const item of array) {
+      if (callback(item)) {
+        return result;
+      }
+      result.push(item);
+    }
+    return result;
+  };
+  
+  const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+  const results1 = takeUntil(data1, x => x < 0);
+  console.log(results1); // [1, 2, 5, 7, 2]
+  
+  const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+  const results2 = takeUntil(data2, x => x === ',');
+  console.log(results2); // ["I've", "been", "to", "Hollywood"]
+  
