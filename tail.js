196ec9713b7822b40646348881bbab0c21bc51c2
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-16 04:12:43 -0500

    tail.js

--- tail.js
+++ tail.js
@@ -21,3 +21,36 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 console.log(tail([]));
+
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
+  }
+
+  return array.slice(1);
+};
+
+module.exports = tail;
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe('#tail', () => {
+  it('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it('should return [] for [1]', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it('should return [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it('should not modify the original array', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"]);
+  });
+});
