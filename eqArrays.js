a0dadf09c3b5fd4672a769a397c714e1377f9d8a
i3lmi <118832707+i3lmi@users.noreply.github.com>
2023-02-16 04:17:07 -0500

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -19,3 +19,11 @@ function eqArrays(array1, array2) {
     return true;
   }
   
+  const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+
+// test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
